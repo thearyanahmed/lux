a07c8ddab5d2af6b0187f82f46569a91fe00759c
Aryan <paryan@digitalocean.com>
2025-11-17 23:10:24 +0000

    Add run command skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::Subcommand;
+use clap::arg;
 use clap::Parser;
 
 mod tasks;
@@ -9,12 +11,26 @@ pub const VERSION: &str = "0.0.1";
 #[command(name = "lux")]
 #[command(version = VERSION)]
 struct CLI {
+    #[command(subcommand)]
+    commands: Commands,
+}
 
+#[derive(Subcommand)]
+enum Commands {
+    Run {
+        #[arg(short = 't', long)]
+        task_id: String,
+    }
 }
 
 fn main() {
-    let _cli = CLI::parse();
+    let cli = CLI::parse();
 
+    match cli.commands {
+        Commands::Run { task_id } => {
+            println!("task id {}", task_id)
+        }
+    }
 }
 
 
